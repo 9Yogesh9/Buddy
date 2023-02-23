bff94655ebd1fec236ccde16ec3660d8d0c0b719
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-02-24 01:33:19 +0530

    Search functionality built

--- buddy_frontend/src/components/Sidebar.jsx
+++ buddy_frontend/src/components/Sidebar.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
+import { categories } from '../utils/data';
 
 import logo from '../assets/logo.png';
 
 const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle = "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-  { name: 'Animals' },
-  { name: 'Wallpapers' },
-  { name: 'Photography' },
-  { name: 'Gaming' },
-  { name: 'Coding' },
-]
 
 const SideBar = ({ user, closeToggle }) => {
 
@@ -54,6 +47,7 @@ const SideBar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
               {category.name}
             </NavLink>
           ))}
